8e49e4b75a0c18be2571dda577240fe308c1ae5f
esmond-adjei <xmongram@gmail.com>
2023-12-07 21:12:03 +0000

    modified some files

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,8 +2,8 @@
 """
 type-annotated function
 """
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """sums lists of integers"""
     return sum(input_list)
