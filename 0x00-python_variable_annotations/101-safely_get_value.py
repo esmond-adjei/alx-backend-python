0ffc993e7ed7edcb44b95d5463044a37e2e6c309
esmond-adjei <xmongram@gmail.com>
2023-12-07 21:48:59 +0000

    modified some files

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,12 +2,14 @@
 """
 Add type annotations
 """
-from typing import Sequence, Any, TypeVar
+from typing import Mapping, Union, Any, TypeVar
 
 
-T = TypeVar('T', bound=Any)
+T = TypeVar('T')
 
-def safely_get_value(dct: dict[Any, Any], key: Any, default: T | None = None) -> Any | T:
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] =
+                     None) -> Union[Any, T]:
     if key in dct:
         return dct[key]
     else:
