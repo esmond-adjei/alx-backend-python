e0463979649dabf762fe5da9b0d94caab744d322
esmond-adjei <xmongram@gmail.com>
2024-01-09 02:46:22 +0000

    modified some files

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,12 +5,12 @@ Parameterize a unit test
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """
-    Tests access_nested_map method of GithubOrgClient
+    Test access_nested_map method of GithubOrgClient
     """
 
     @parameterized.expand([
@@ -26,8 +26,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError),
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_exception):
-        with self.assertRaises(expected_exception):
+    def test_access_nested_map_exception(self, nested_map, path, exception):
+        with self.assertRaises(exception):
             access_nested_map(nested_map, path)
 
 
@@ -45,3 +45,32 @@ class TestGetJson(unittest.TestCase):
             mock_get.return_value.json.return_value = test_payload
             self.assertEqual(get_json(test_url), test_payload)
             mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test memoize decorator
+    """
+    def test_memoize(self):
+        """
+        Using patch, verify that calling a_property twice
+        returns the correct result, while ensuring
+        that a_method is only called once
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        t = TestClass()
+        with patch.object(t, 'a_method', return_value=42) as mock_method:
+            result1 = t.a_property
+            result2 = t.a_property
+
+            mock_method.assert_called_once()
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
