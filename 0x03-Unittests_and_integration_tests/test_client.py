2afababac480c339b704db0b54d67f669d71a406
esmond-adjei <xmongram@gmail.com>
2024-01-09 03:24:27 +0000

    modified some files

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Unit tests for client.py
+"""
+from typing import Dict
+import unittest
+from unittest.mock import MagicMock, patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Tests for GithubOrgClient class."""
+    @parameterized.expand([
+        ("google", {'post': "google"}),
+        ("abc", {'post': "abc"}),
+    ])
+    @patch(
+        "client.get_json",
+    )
+    def test_org(self, t_org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
+        """Tests the org method."""
+        mocked_fxn.return_value = MagicMock(return_value=resp)
+        gh_org_client = GithubOrgClient(t_org)
+        self.assertEqual(gh_org_client.org(), resp)
+        mocked_fxn.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(t_org)
+        )
