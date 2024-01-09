cf1c819f198a0325f63a7ecae1bfd53255634e55
esmond-adjei <xmongram@gmail.com>
2024-01-09 03:05:39 +0000

    modified some files

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -54,15 +54,15 @@ class TestMemoize(unittest.TestCase):
     def test_memoize(self) -> None:
         """Test memoize decorator by mocking a_class method"""
         class TestClass:
+
             def a_method(self):
                 return 42
 
             @memoize
             def a_property(self):
                 return self.a_method()
-        with patch.object(TestClass, "a_method",
-                          return_value=lambda: 42) as a_method:
-            test_class = TestClass()
-            self.assertEqual(test_class.a_property(), 42)
-            self.assertEqual(test_class.a_property(), 42)
+        with patch.object(TestClass, 'a_method') as a_method:
+            test = TestClass()
+            test.a_property()
+            test.a_property()
             a_method.assert_called_once()
